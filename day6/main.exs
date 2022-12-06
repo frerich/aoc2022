cb4f220375f940697eac7b36fe811fd8ac2a80e1
Frerich Raabe <frerich.raabe@gmail.com>
2022-12-06 09:21:35 +0100

    Simplified today's solution a bit

--- day6/main.exs
+++ day6/main.exs
@@ -1,11 +1,11 @@
 defmodule Day6 do
   def part_one(input) do
     chunks = Enum.chunk_every(to_charlist(input), 4, 1)
-    Enum.find_index(chunks, & Enum.count(Enum.uniq(&1)) == 4) + 4
+    Enum.find_index(chunks, & Enum.uniq(&1) == &1) + 4
   end
 
   def part_two(input) do
     chunks = Enum.chunk_every(to_charlist(input), 14, 1)
-    Enum.find_index(chunks, & Enum.count(Enum.uniq(&1)) == 14) + 14
+    Enum.find_index(chunks, & Enum.uniq(&1) == &1) + 14
   end
 end
